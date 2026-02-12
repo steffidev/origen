5daffde6e488b44b6695fd0bd388d434a2ab0b66
Steffi_dev <estefaniam.formacion@gmail.com>
2026-02-12 16:54:03 +0100

    últimos cambios

--- js/ayuda.js
+++ js/ayuda.js
@@ -1,6 +1,6 @@
-window.addEventListener("load", () => {
-    document.addEventListener("keydown", (evento) => {
-        if (evento.shiftKey && evento.key.toUpperCase() === 'H') {
+window.addEventListener("load", function() {
+    document.addEventListener("keydown", function(evento) {
+        if (evento.shiftKey && evento.key.toUpperCase() === 'Q') {
             window.location.href = "contacto.html";
         }
     });
